1fe71c33f35f94cc2dc2364ebbf68f15de8370b2
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-26 18:48:53 -0300

    feat: estrutura inicial da Intro do componente Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,28 @@
+import { IntroBlock, HomeContainer } from "./styles";
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react";
+
+import imgCoffee from '../../assets/img-coffee.png';
+
 export function Home() {
   return (
-    <h1>Home</h1>
+    <HomeContainer>
+      <IntroBlock>
+        <div>
+          <header>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+          </header>
+
+          <ul>
+            <li><span><ShoppingCart /></span> Compra simples e segura</li>
+            <li><span><Package /></span> Embalagem mantém o café intacto</li>
+            <li><span><Timer /></span> Entrega rápida e rastreada</li>
+            <li><span><Coffee /></span> O café chega fresquinho até você</li>
+          </ul>
+        </div>
+
+        <img src={imgCoffee} alt="Copo de café" />
+      </IntroBlock>
+    </HomeContainer>
   );
 }
