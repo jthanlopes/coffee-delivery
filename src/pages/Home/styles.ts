f7a2cf23f4e75f4f5fa5569771b28741d465dbcf
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-26 18:49:14 -0300

    style: início da estilização da Home

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+export const IntroBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div header {
+
+  }
+
+  div ul {
+
+  }
+`
